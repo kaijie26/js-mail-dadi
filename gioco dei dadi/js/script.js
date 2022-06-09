7ae414ddbd64c580443fe637a97c4ac4c1e23722
kaijie26 <chenkaijie26@gmail.com>
2022-06-09 10:23:15 +0200

    fix

--- gioco dei dadi/js/script.js
+++ gioco dei dadi/js/script.js
@@ -2,19 +2,21 @@
 // Stabilire il vincitore, in base a chi fa il punteggio più alto.
 
 // Genero un numero random per l'utente
-let userNumber = Math.floor(Math.random() * 7);
+const userNumber = Math.floor(Math.random() * 7);
 alert(`il tuo numero è ${userNumber}`);
 
 // Genero un numero random per il computer
-let computer = Math.floor(Math.random() * 7);
+const computer = Math.floor(Math.random() * 7);
 alert(`il numero del computer è ${computer}`);
 
 // Stabilisco il vicitore fra l'utente e il computer
 let message;
 if(userNumber > computer) {
     message = `Complimenti hai vinto`;
-}else{
+}else if(userNumber < computer){
     message = `Mi dispiace ha vinto il computer`;
+}else{
+    message = `La partita è patta`;
 }
 // Stampo il vincitore
 alert(message);
